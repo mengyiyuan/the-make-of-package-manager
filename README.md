5b603c3b7fdff1d40b1a784ce2f49b0f9baae99d
mengyiyuan <mengyi0614@gmail.com>
2017-07-20 16:39:36 +0800

    Initial commit with Chapter 1,2

---
+++ README.md
@@ -0,0 +1,8 @@
+### The make of a package manager
+
+Following tutorial from [Let's Dev a Package Manager](https://yarnpkg.com/blog/2017/07/11/lets-dev-a-package-manager/)
+
+To run the application:
+```
+npm start
+```
---
+++ index.js
@@ -0,0 +1,38 @@
+import fetch from 'node-fetch'
+import semver from 'semver'
+import fs from 'fs-extra'
+
+async function getPinnedReference({name, reference}) {
+  // only require extra process if input reference is not pinned
+  if (semver.validRange(reference) && !semver.valid(reference)) {
+    let response = await fetch(`https://registry.yarnpkg.com/${name}`)
+    let info = await response.json()
+
+    let versions = Object.keys(info.versions)
+    let maxSatisfying = semver.maxSatisfying(versions, reference)
+
+    if (maxSatisfying == null) {
+      throw new Error(`Couldn't find a version matching "${reference}" for package "${name}"`)
+    }
+
+    reference = maxSatisfying
+  }
+
+  return {name, reference}
+}
+
+console.log(getPinnedReference({name: "react", reference: "~15.3.0"}))
+
+async function fetchPackage({name, reference}) {
+
+  if([`/`, `./`, `../`].some((prefix) => reference.startsWith(prefix)))
+    return await fs.readFile(reference)
+
+  if(semver.valid(reference))
+    return await fetchPackage({name, reference: `https://registry.yarnpkg.com/${name}/-/${name}-${reference}.tgz`})
+
+  let response = await fetch(reference)
+  if(!response.ok)
+    throw new Error(`Couldn't fetch package "${reference}"`)
+  return await response.buffer()
+}
